b205dd4f27b878ef660e846d1d1ea86b96d93e42
Anh Tuan Vu <avu@united.domain>
2019-08-16 10:34:18 +0200

    Added docstring.

--- utils.py
+++ utils.py
@@ -1,2 +1,5 @@
 def select_columns(columns, labels):
+    """
+    Return the positions of the elements in ``labels`` inside ``columns``
+    """
     return [pos for pos in range(len(columns)) if columns[pos] in labels]
